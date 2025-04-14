dd637d928fbb9782faee4727bf9f1466c534bbf0
hatim997 <laravel.supersoft@gmail.com>
2025-04-14 11:38:40 +0500

    test

--- resources/views/frontend/components/deals.blade.php
+++ resources/views/frontend/components/deals.blade.php
@@ -1,20 +1,21 @@
 <section class="deal-wrap">
     <div class="container-xxl">
       <div class="title-prt">
-        <h2>Deal of the day</h2>
+        <h2>À la Carte</h2>
+        {{-- <h2>Deal of the day</h2> --}}
         <a href="{{route('ecommerce')}}" class="btn btn-outline-warning">View All <i class="fa-solid fa-arrow-right"></i></a>
       </div>
 
       <div class="deal-slider owl-carousel mt-5 slider">
         @if (!empty($products))
-                
+
          @foreach ($products as $product)
             <div class="item" data-aos="fade-up" data-aos-duration="1500">
                 <div class="deal-bx">
                     <a href="{{ route('ecom.details', $product->productId) }}">
                         <div class="deal-img">
-                           <img class="img-fluid" 
-     src="{{ $product->ecommerce()->first()?->images()->first()?->image ? url('storage/' . $product->ecommerce()->first()->images()->first()->image) : asset('assets/frontend/images/logo.png') }}" 
+                           <img class="img-fluid"
+     src="{{ $product->ecommerce()->first()?->images()->first()?->image ? url('storage/' . $product->ecommerce()->first()->images()->first()->image) : asset('assets/frontend/images/logo.png') }}"
      alt="Product Image" />
                             {{-- <span class="hot-tag">Hot <br />Deal</span> --}}
                         </div>
@@ -28,7 +29,7 @@
          @endforeach
         @endif
 
-        
+
       </div>
     </div>
 </section>
